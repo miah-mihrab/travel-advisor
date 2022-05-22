a1c791f3e556e641f7260f4018f2330fab66ddfa
miah-mihrab <miah.mihrab@gmail.com>
2022-05-22 14:40:32 +0600

    map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
+import useStyles from './styles';
 const Map = () => {
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinate = {lat: 0, lng: 0};
     return (
-        <div className="map">
-          <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: "AIzaSyCukNp5ROfEz2VDilDt8SLt872TfvoDNw8"}}
+                defaultCenter={coordinate}
+                center={coordinate}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </div>
     );
 }
